5d513669afeadf9a5783bb09b40126d4ae25c9e3
Tarun <113366877+tarunl2002@users.noreply.github.com>
2024-04-07 11:43:44 +0530

    Time: 0 ms (100.00%) | Memory: 7.3 MB (75.32%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.cpp
+++ 678-valid-parenthesis-string/valid-parenthesis-string.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     bool checkValidString(string s) {
-        int low = 0;
-        int high = 0;
+        int open = 0, close = 0;
+        for(auto i : s) {
+            if(i == ')') {
+                open--;
+            }else {
+                open++;
+            }
+            if(open < 0) {
+                return false;
+            }
 
-        for (char c : s) {
-            low += (c == '(') ? 1 : -1;
-            high += (c != ')') ? 1 : -1;
-
-            if (high < 0) break;
-            low = max(low, 0);
         }
 
-        return low == 0;
+        for(int i = s.length() - 1; i>= 0; i--) {
+            if(s[i] == '(') {
+                close--;
+            }else {
+                close++;
+            }
+
+            if(close < 0) {
+                return false;
+            }
+        }
+
+        return true;
     }
 };
