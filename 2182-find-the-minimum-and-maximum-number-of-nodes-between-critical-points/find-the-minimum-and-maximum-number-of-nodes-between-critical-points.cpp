14811fa4ddc23c878d717ac09e8a8a8e6b2d2668
Tarun <113366877+tarunl2002@users.noreply.github.com>
2024-07-05 12:26:25 +0530

    Time: 121 ms (98.17%) | Memory: 119.6 MB (35.95%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -11,13 +11,11 @@
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-                ios_base::sync_with_stdio(false);
-
-        vector<int> res;
+        ios_base::sync_with_stdio(false);
         vector<int> temp;
-            int mi=9999999;
-        int c = 0;
+        int mi=9999999;
         if(head->next==nullptr || head->next->next==nullptr) return {-1,-1};
+        int c = 0;
         while (head->next->next != nullptr) {
             c++;
             if ((head->next->val > head->val &&
