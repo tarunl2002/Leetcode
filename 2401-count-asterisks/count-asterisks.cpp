f248734e1d8373d96a221b6c255ade544bbe778a
Tarun <113366877+tarunl2002@users.noreply.github.com>
2024-08-20 13:45:32 +0530

    Time: 4 ms (49.83%) | Memory: 7.7 MB (72.03%) - LeetSync

--- 2401-count-asterisks/count-asterisks.cpp
+++ 2401-count-asterisks/count-asterisks.cpp
@@ -4,10 +4,7 @@ public:
         int count=0;
         bool open=false;
         for(int i=0;i<s.length();i++){
-            if(open==false && s[i]=='*'){
-                count++;
-
-            } 
+            if(open==false && s[i]=='*') count++;
             if(s[i]=='|'){
                 if(open==true) open=false;
                 else open=true;
