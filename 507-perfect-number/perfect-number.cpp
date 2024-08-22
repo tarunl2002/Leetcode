e4aa1be966dec008ee7cbe1e4274221ac690f90f
Tarun <113366877+tarunl2002@users.noreply.github.com>
2024-08-22 11:28:58 +0530

    Time: 2075 ms (27.04%) | Memory: 7.4 MB (41.74%) - LeetSync

---
+++ 507-perfect-number/perfect-number.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int c=0;
+        for(int i=1;i<num;i++){
+            if(num%i==0) c+=i;
+        }
+        if(c==num) return true;
+        else return false;
+    }
+};
