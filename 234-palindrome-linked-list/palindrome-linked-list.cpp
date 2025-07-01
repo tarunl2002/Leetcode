c435c2f95b9909ce76656df9c9766e9e24666721
Tarun <113366877+tarunl2002@users.noreply.github.com>
2025-07-01 16:12:45 +0530

    Time: 84 ms (5.05%) | Memory: 125 MB (34.40%) - LeetSync

---
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    bool isPalindrome(ListNode* head) {
+        stack<int> st;
+        ListNode* slow=head;
+        ListNode* fast=head;
+        while(fast!=NULL && fast->next != NULL){
+            st.push(slow->val);
+            slow=slow->next;
+            fast=fast->next->next;
+        }
+        if(fast!=NULL) st.push(slow->val);
+        while(!st.empty() && slow!=NULL){
+            if(st.top()==slow->val){
+                cout<<st.top();
+                st.pop();
+                slow=slow->next;
+            }
+            else break;
+        }
+        return st.empty() ? true : false;
+    }
+};
