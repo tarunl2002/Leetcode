ee2714493916e3421e4a9909981e328e600ec219
Tarun <113366877+tarunl2002@users.noreply.github.com>
2025-07-02 19:32:42 +0530

    Time: 37 ms (89.11%) | Memory: 94 MB (41.01%) - LeetSync

--- 435-non-overlapping-intervals/non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/non-overlapping-intervals.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int eraseOverlapIntervals(vector<vector<int>>& v) {
-        sort(v.begin(), v.end(), [&] (vector<int> &a, vector<int> &b){
-            if(a[1]==b[1]){
-                return a[0]<b[0];
-            }
-            return a[1]<b[1];
+    int eraseOverlapIntervals(vector<vector<int>>& arr) {
+        sort(arr.begin(), arr.end(),[]( const vector<int>& a, const vector<int>& b){
+            return a[1]<b[1]; 
         });
-        int r=0;
-        int end=v[0][1];
-        for(int i=1;i<v.size();i++){
-            if(v[i][0]<end){
-                r++;
+        int c=0;
+        int start=arr[0][0];
+        int end = arr[0][1]; 
+        for(int i=1; i<arr.size();i++){
+            if(end>arr[i][0]){
+                c++;
             }
             else{
-                end=v[i][1];
+                start=arr[i][0];
+                end=arr[i][1];
             }
-
         }
-        return r;
+        return c;
     }
 };
